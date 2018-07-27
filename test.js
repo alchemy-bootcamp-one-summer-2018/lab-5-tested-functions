bc48ca05d2767b281bb3ab962309cac962c8dd02
Mackenzie Berliner-Glasser <mack.bg94@gmail.com>
2018-07-27 10:02:25 -0700

    try catch

--- test.js
+++ test.js
@@ -1,3 +1,26 @@
 
+function passingTest() {
 
-bar;
+}
+
+try {
+    passingTest();
+    console.log('test PASSED');
+}
+catch(err) {
+    console.log('test FAILED', err.message);
+    
+}
+
+function failingTest() {
+    throw new Error ('assertion error');
+}
+
+try {
+    failingTest();
+    console.log('test PASSED');
+}
+catch(err) {
+    console.log('test FAILED', err.message);
+    
+}
