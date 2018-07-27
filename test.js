e5ebca858b853c752ad1b95f9d935bb41ca5314e
Sophie Graham <sophiergraham1@gmail.com>
2018-07-27 10:02:58 -0700

    added test

--- test.js
+++ test.js
@@ -1,3 +1,23 @@
+function passingTest() {
 
+}
 
-bar;
+try {
+    passingTest();
+    console.log('test PASSED');
+}
+catch(err){
+    console.log('Test FAILED', err.message);
+}
+
+function failingTest() {
+    throw new Error('test PASSED');
+}
+
+try {
+    failingTest();
+    console.log('test PASSED'); 
+}
+catch(err){
+    console.log('test FAILED', err.message);
+}
