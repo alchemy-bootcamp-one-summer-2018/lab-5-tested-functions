972064a0f7d2055d2a8f7be0fda7995917165820
Jack <ajackofsometrades@gmail.com>
2018-07-27 10:02:24 -0700

    set up try/catch block

--- test.js
+++ test.js
@@ -1,3 +1,30 @@
 
 
-bar;
+
+function passingTest(){
+
+}
+
+function failingTest(){
+    throw new Error('assertion error');
+}
+
+
+try {
+    passingTest()
+    console.log('Test PASSED');
+}
+
+
+catch (err) {
+    console.log('Test FAILED', err.message);
+}
+
+try {
+    failingTest();
+    console.log('Test FAILED');
+}
+
+catch(err) {
+    console.log('Test FAILED', err.message);
+}
