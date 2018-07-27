76a51b20f6e5e6b812e61e5a509e3fe97208ac0e
Claire Flanagan <claireflanagan@gmail.com>
2018-07-27 10:02:15 -0700

    added two tests

--- test.js
+++ test.js
@@ -1,3 +1,26 @@
+function passingTest() {
+}
 
+try {
+    passingTest();
+    console.log('TEST PASSED');
+    
+}
 
-bar;
+catch(err) {
+    console.log('TEST FAILED', err.message);
+}
+
+function failingTest() {
+    throw new Error('assertion error');
+}
+
+try {
+    failingTest();
+    console.log('TEST PASSED');
+    
+}
+
+catch(err) {
+    console.log('TEST FAILED', err.message);
+}
