1d80edabf3dae0513d6c46a5c26b1b373dbc6847
Sophie Graham <sophiergraham1@gmail.com>
2018-07-27 11:46:25 -0700

    test ran

--- test.js
+++ test.js
@@ -1,23 +1,82 @@
-function passingTest() {
+/* globals assert */
 
-}
+var tests = [];
 
-try {
-    passingTest();
-    console.log('test PASSED');
-}
-catch(err){
-    console.log('Test FAILED', err.message);
-}
-
-function failingTest() {
-    throw new Error('test PASSED');
+function test(name, testFn) {
+    tests.push({
+        name:name,
+        testFn: testFn
+    });
 }
 
-try {
-    failingTest();
-    console.log('test PASSED'); 
+test.run = function() {
+    var results = runTests();
+    displayResults(results);
+};
+
+function runTests() {
+    var results = [];
+    var test;
+    for(var i = 0; i < tests.length; i++) {
+        test = tests[i];
+        
+        try {
+            test.testFn();
+            // if no errors - passes
+            test.status = 'PASSED';
+        }
+        catch(err) {
+            // if errors - fails
+            test.status = 'FAILED';
+            test.error = err.message;
+        }
+
+        // record the results
+        results.push(test);
+
+       
+    }
+     // return all results
+    return results;
 }
-catch(err){
-    console.log('test FAILED', err.message);
+function displayResults(results) {
+    var result;
+    var color;
+    
+    for(var i = 0; i < results.length; i++) {
+        result = results[i];
+        
+        if(result.status === 'PASSED'){
+            color = 'green';
+        }
+        else {
+            color = 'red';
+        }
+
+        console.log(
+            result.name + '%c' + result.status,
+            'color: ' + color + ';'
+        );
+
+        if(result.status === 'FAILED') {
+            console.log (
+                '\t%c' + (result.error || ''),
+                'color: red;'
+            );
+        }   
+    }
+}
+// change to true to run test tests
+if(false) { // eslint-disable-line
+
+
+    test('1 + 1 = 2 passes', function() {
+        assert.equal(1 + 1, 2);
+    });
+
+    test('1 + 1 = 2 fails', function() {
+        assert.equal(1 + 1, 3);
+    });
+
+    test.run();
 }
